781f7dcc431ea19d79cd0c953ebe2427bf028125
Eli Martin <elizabeth.k.martin-1@ou.edu>
2020-11-09 16:48:44 -0600

    added a main and an outline for getSize method

--- FileSize.java
+++ FileSize.java
@@ -1,5 +1,34 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class FileSize
 {
-
+	/*
+	 * Directory
+	 * |
+	 * | file 2 file2 file3 dir1
+	 *                       |
+	 *                       file4 file5 dir2
+	 *                                    |
+	 *                                    file6
+	 */
+	
+	public static long getSize(File file)
+	{
+		long size = 0;
+		// if file -> get size
+		// if directory -> get all of the files
+		// 
+		
+		
+		return size;
+	}
+	
+	public static void main(String [] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please give me a directory for a file.");
+		String str = input.nextLine();
+		System.out.println("The size is: " + getSize(new File(str))); //creates a new file from the string; could also be a directory
+	}
 }
